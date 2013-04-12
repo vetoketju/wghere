477641ba54cb4f706b6a01dbc431278d24e3d0d7
vetoketju <ville.valta@gmail.com>
2013-04-12 17:03:14 +0300

    Initial commit

---
+++ httpDownloader/VSWGet.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace httpDownloader
+{
+    partial class VSWGet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.dlbt = new System.Windows.Forms.Button();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 40);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(492, 19);
+            this.progressBar.TabIndex = 0;
+            // 
+            // dlbt
+            // 
+            this.dlbt.Location = new System.Drawing.Point(393, 12);
+            this.dlbt.Name = "dlbt";
+            this.dlbt.Size = new System.Drawing.Size(111, 23);
+            this.dlbt.TabIndex = 1;
+            this.dlbt.Text = "Download";
+            this.dlbt.UseVisualStyleBackColor = true;
+            this.dlbt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // urlBox
+            // 
+            this.urlBox.Location = new System.Drawing.Point(12, 12);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(375, 22);
+            this.urlBox.TabIndex = 2;
+            // 
+            // VSWGet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 78);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.dlbt);
+            this.Controls.Add(this.progressBar);
+            this.Name = "VSWGet";
+            this.Text = "VerySimpleWGet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button dlbt;
+        private System.Windows.Forms.TextBox urlBox;
+    }
+}
+
---
+++ httpDownloader/VSWGet.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace httpDownloader
+{
+    public partial class VSWGet : Form
+    {
+        public string url = "";
+        public string filename = "httpDownloaderFile";
+        public string outputfolder = "";
+        string[] args;
+
+        public VSWGet(string[] args)
+        {
+            this.args = args;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pathUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string pathDownload = Path.Combine(pathUser, "Downloads");
+
+            if (args.Length < 1 || !Directory.Exists(args[0]))
+            {
+                DialogResult res = MessageBox.Show("No output folder specified. Downloading to: "+pathDownload, "Warning", MessageBoxButtons.OKCancel);
+                if (res != DialogResult.OK)
+                {
+                    Application.Exit();
+                    return;
+                }
+                outputfolder = pathDownload;
+            }
+            else outputfolder = args[0];
+
+            if (!outputfolder.EndsWith("/"))
+                outputfolder += "/";
+
+            System.Diagnostics.Debug.WriteLine(pathDownload);
+
+            if (urlBox.Text.Trim() != "" && urlBox.Text.Trim().StartsWith("http://"))
+            {
+                url = urlBox.Text.Trim();
+                filename = url.Substring(url.LastIndexOf("/")+1);
+          
+                if (File.Exists(outputfolder + filename))
+                    filename = string.Format("{0:yyyy-MM-dd_HH-mm-ss}", DateTime.Now) + filename;
+
+                try
+                {
+                    WebClient wc = new WebClient();
+                    wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+                    wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                    wc.DownloadFileAsync(new Uri(url),outputfolder+filename);
+                }
+                catch (Exception er)
+                {
+                    DialogResult res = MessageBox.Show(er.ToString(), "Error occured:", MessageBoxButtons.OK);
+                    Application.Exit();
+                    return;
+                }
+            }else  MessageBox.Show("Bad url");
+         
+
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
